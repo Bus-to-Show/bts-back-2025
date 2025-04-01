20304e8c12423291984be60d97057e670435dbd0
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-04-01 17:38:42 -0600

    Clean up seed files and make them useful

--- seeds/007_reservations.js
+++ seeds/007_reservations.js
@@ -3,10 +3,26 @@ exports.seed = (knex) => {
   return knex('reservations').del()
     .then(() => {
       // Inserts seed entries
-      return knex('reservations').insert(
-      )
+      return knex('reservations').insert([
+        {
+          id: 1,
+          orderId: 1,
+          pickupPartiesId: 1,
+          willCallFirstName: 'Dave',
+          willCallLastName: 'Kennedy',
+          status: 1,
+        },
+        {
+          id: 2,
+          orderId: 1,
+          pickupPartiesId: 1,
+          willCallFirstName: 'Dave',
+          willCallLastName: 'Kennedy',
+          status: 1,
+        },
+      ]);
     })
     .then(() => {
-      return knex.raw("SELECT setval('reservations_id_seq', (SELECT MAX(id) FROM reservations))")
-    })
-}
+      return knex.raw("SELECT setval('reservations_id_seq', (SELECT MAX(id) FROM reservations))");
+    });
+};
