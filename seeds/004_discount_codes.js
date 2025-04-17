5242d26d6fbca7713ec2d4a9e6510b3a16cb46a7
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-04-17 01:02:40 -0600

    More seeds

--- seeds/004_discount_codes.js
+++ seeds/004_discount_codes.js
@@ -6,17 +6,26 @@ exports.seed = (knex) => {
       return knex('discount_codes').insert([
         {
           id: 1,
-          discountCode: 'SAVE20',
-          percentage: 20,
+          discountCode: '1FREE',
           expiresOn: 20251231,
           issuedOn: 20250101,
           issuedTo: 'BTS staff',
           issuedBy: 'DK',
           issuedBecause: 'Feeling generous',
-          timesUsed: 3,
           type: 1,
+        },
+        {
+          id: 2,
+          discountCode: 'SAVE20',
+          percentage: 20,
+          expiresOn: 20251231,
+          issuedOn: 20250101,
+          issuedTo: 'FOTM staff',
+          issuedBy: 'DK',
+          issuedBecause: 'Pickup location',
+          timesUsed: 3,
+          type: 2,
           remainingUses: 2,
-          usesPerEvent: 3,
         },
       ]);
     })
