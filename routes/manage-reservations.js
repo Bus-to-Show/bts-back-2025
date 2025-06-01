3277e644dae2d80e2c988574a23c2b7bba9cb642
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-06-01 17:53:34 -0600

    Formatting

--- routes/manage-reservations.js
+++ routes/manage-reservations.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const express = require('express');
 const router = express.Router();
 
@@ -6,145 +7,116 @@ const router = express.Router();
 const parse = require("pg-connection-string").parse;
 const pgconfig = parse(process.env.DATABASE_URL);
 
-const Pool = require('pg').Pool
-const pool = new Pool(pgconfig)
+const Pool = require('pg').Pool;
+const pool = new Pool(pgconfig);
 
 //List (get all of the reservations by location)
-router.get('/:id', (req, res, next) => {
-    pool.connect((err, client, release) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({message: 'An unknown error occurred.'});
-          return;
-        }
-
-        client.query(`
-
-        SELECT * FROM reservations where reservations."pickupPartiesId" = ${req.params.id}
-        ORDER BY reservations."willCallLastName", reservations."willCallFirstName", reservations."id" 
-        
-        `, (err, result) => {
-          release()
-
-          if (err) {
-            console.error(err);
-            res.status(500).json({message: 'An unknown error occurred.'});
-            return;
-          }
-
-          res.status(200).json(result.rows)
-        })
-      })
-
-  })
-
-  //List (get all of the orders by party)
-router.get('/orders-by-party/:id', (req, res, next) => {
+router.get('/:id', (req, res) => {
   pool.connect((err, client, release) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'An unknown error occurred.'});
+      return;
+    }
+
+    client.query(`
+      SELECT * FROM reservations where reservations."pickupPartiesId" = ${req.params.id}
+      ORDER BY reservations."willCallLastName", reservations."willCallFirstName", reservations."id"
+    `, (err, result) => {
+      release();
+
       if (err) {
         console.error(err);
         res.status(500).json({message: 'An unknown error occurred.'});
         return;
       }
 
-     const query =  `
-        SELECT reservations.id as id, reservations.* , orders."orderedByFirstName", orders."orderedByLastName", orders."orderedByEmail", orders."userId"  FROM reservations
-        JOIN orders on reservations."orderId" = orders.id 
-        where reservations."pickupPartiesId" = ${req.params.id}
-        ORDER BY orders."orderedByLastName", orders."orderedByFirstName", reservations."willCallLastName", reservations."willCallFirstName", reservations."id"  
-      `
-    
-      console.log('/orders-by-party/:id hit!!!! ', query)
-      client.query(query, (err, result) => {
-        release()
+      res.status(200).json(result.rows);
+    });
+  });
+});
 
-        if (err) {
-          console.error(err);
-          res.status(500).json({message: 'An unknown error occurred.'});
-          return;
-        }
+//List (get all of the orders by party)
+router.get('/orders-by-party/:id', (req, res) => {
+  pool.connect((err, client, release) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'An unknown error occurred.'});
+      return;
+    }
 
-        res.status(200).json(result.rows)
-      })
-    })
+    const query =  `
+      SELECT reservations.id as id, reservations.* , orders."orderedByFirstName", orders."orderedByLastName", orders."orderedByEmail", orders."userId"  FROM reservations
+      JOIN orders on reservations."orderId" = orders.id
+      where reservations."pickupPartiesId" = ${req.params.id}
+      ORDER BY orders."orderedByLastName", orders."orderedByFirstName", reservations."willCallLastName", reservations."willCallFirstName", reservations."id"
+    `;
 
-})
+    client.query(query, (err, result) => {
+      release();
 
+      if (err) {
+        console.error(err);
+        res.status(500).json({message: 'An unknown error occurred.'});
+        return;
+      }
 
-  router.patch('/:id', function(req, res, next){
-    pool.connect((err, client, release) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({message: 'An unknown error occurred.'});
-          return;
-        }
+      res.status(200).json(result.rows);
+    });
+  });
+});
 
-        client.query(` 
-            UPDATE reservations
-            SET status = ${req.body.status}
-            WHERE id = ${req.params.id}
-            RETURNING *
-        ;
-        `, (err, result) => {
-          release()
+router.patch('/:id', (req, res) => {
+  pool.connect((err, client, release) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'An unknown error occurred.'});
+      return;
+    }
 
-          if (err) {
-            console.error(err);
-            res.status(500).json({message: 'An unknown error occurred.'});
-            return;
-          }
+    client.query(`
+      UPDATE reservations
+      SET status = ${req.body.status}
+      WHERE id = ${req.params.id}
+      RETURNING *
+    `, (err, result) => {
+      release();
 
-          res.status(200).json(result.rows)
-        })
-      })
-  })
+      if (err) {
+        console.error(err);
+        res.status(500).json({message: 'An unknown error occurred.'});
+        return;
+      }
 
-  router.put('/:id', function(req, res, next){
-    pool.connect((err, client, release) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({message: 'An unknown error occurred.'});
-          return;
-        }
+      res.status(200).json(result.rows);
+    });
+  });
+});
 
-        // const partyBody = {
-        //     id: req.body.party_id,
-        //     eventId: req.body.eventid,
-        //     pickupLocationId: req.body.location_id,
-        //     lastBusDepartureTime: req.body.lastBusDepartureTime,
-        //     firstBusLoadTime: req.body.firstBusLoadTime,
-        //     partyPrice: req.body.partyPrice,
-        //     capacity: req.body.capacity,
-        //   }
-          
-        //   if(partyBody.capacity == null) partyBody.capacity = 0
-        //   if(partyBody.partyPrice == null) partyBody.partyPrice = 30
-        //   if(partyBody.lastBusDepartureTime == null) partyBody.lastBusDepartureTime = '17:30'
-        //   if(partyBody.firstBusLoadTime == null) partyBody.firstBusLoadTime = partyBody.lastBusDepartureTime
+router.put('/:id', (req, res) => {
+  pool.connect((err, client, release) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'An unknown error occurred.'});
+      return;
+    }
 
-        // const {id, eventId, pickupLocationId, lastBusDepartureTime, firstBusLoadTime, partyPrice, capacity, created_at, updated_at} = req.body
-        const updateQuery = `
+    const updateQuery = `...`;
+    const insertQuery = `...`;
+    const homeMadeUpsertQuery = partyBody.id ?  updateQuery : insertQuery;
 
-                ;`
+    client.query(`${homeMadeUpsertQuery}`, (err, result) => {
+      release();
 
-        const insertQuery = `
+      if (err) {
+        console.error(err);
+        res.status(500).json({message: 'An unknown error occurred.'});
+        return;
+      }
 
-                ;`
-        const homeMadeUpsertQuery = partyBody.id ?  updateQuery : insertQuery; 
-        client.query(`${homeMadeUpsertQuery}`, (err, result) => {
-          release()
+      res.status(200).json(result.rows);
+    });
+  });
+});
 
-          if (err) {
-            console.error(err);
-            res.status(500).json({message: 'An unknown error occurred.'});
-            return;
-          }
-
-          res.status(200).json(result.rows)
-        })
-
-      })
-  })
-
-
-  module.exports = router;
+module.exports = router;
