712d16370a0a3ca150d650d3d61f9f80860fb03c
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-07-06 11:19:39 -0600

    Simplify knex files

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,10 @@
-// Update with your config settings.
+'use strict';
+
+const parse = require('pg-connection-string').parse;
+
 require('dotenv').config();
 
-// in your knexfile.js
-// Should come with install of pg 
-const parse = require("pg-connection-string").parse;
-// Parse the environment variable into an object
-const pgconfig = parse(process.env.DATABASE_URL);
-// Add SSL setting to default environment variable
-
 module.exports = {
-
-  development: {  
-    client: "pg",  
-    connection: pgconfig
-    
-    // client: 'pg',
-    // connection: {
-    //   host: process.env.DATABASE_URL,
-    //   user: process.env.DATABASE_URL,
-    //   password: process.env.DATABASE_URL,
-    //   database: process.env.DATABASE_URL,
-    //   ssl: {rejectUnauthorized: false}
-  },
-  // development: {
-  //   client: 'pg',
-  //   connection: 'postgresql://localhost:5432/bts_q3'
-  // },
-  test: {
-    client: 'pg',
-    connection: 'postgresql://localhost:5432/bts_q3_test'
-  },
-  production: {
-    client: 'pg',
-    connection: pgconfig
-  },
-}
-
-
+  client: 'pg',
+  connection: parse(process.env.DATABASE_URL),
+};
