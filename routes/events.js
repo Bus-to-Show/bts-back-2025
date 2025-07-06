8c8949bfe40c62ab547c6cf997a543f384aab84c
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-07-06 11:27:41 -0600

    Formatting

--- routes/events.js
+++ routes/events.js
@@ -2,71 +2,60 @@
 
 const express = require('express');
 const router = express.Router();
-const knex = require('../knex.js')
+const knex = require('../knex.js');
 
 // Get all
-router.get('/', function(req, res, next){
+router.get('/', (req, res, next) => {
   knex('events')
     .select('id', 'date', 'startTime', 'venue', 'headliner', 'support1', 'support2', 'support3', 'headlinerImgLink', 'headlinerBio', 'meetsCriteria', 'isDenied', 'external')
-    .then((data) => {
-      res.status(200).json(data)
-    })
-})
+    .then(data => res.status(200).json(data));
+});
 
 // Get upcoming
-router.get('/upcoming', function(req, res, next){
+router.get('/upcoming', (req, res, next) => {
   const today = new Date().toLocaleDateString('en-US');
 
   knex('events')
     .select('id', 'date', 'startTime', 'venue', 'headliner', 'support1', 'support2', 'support3', 'headlinerImgLink', 'headlinerBio', 'meetsCriteria', 'isDenied', 'external')
     .whereRaw('date::date >= ?', today)
     .orderByRaw('date::date')
-    .then((data) => {
-      res.status(200).json(data)
-    })
-})
+    .then(data => res.status(200).json(data));
+});
 
-//Read (get one of the resource)
-// Get One
-router.get('/:id', function(req, res, next){
+// Get one
+router.get('/:id', (req, res, next) => {
   knex('events')
     .select('id', 'date', 'startTime', 'venue', 'headliner', 'support1', 'support2', 'support3', 'headlinerImgLink', 'headlinerBio', 'meetsCriteria', 'isDenied', 'external')
     .where('id', req.params.id)
-  .then((data) => {
-    res.status(200).json(data[0])
-  })
-})
+    .then(data => res.status(200).json(data[0]));
+});
 
-//Create (create one of the resource)
-router.post('/', function(req, res, next){
-  if (!req.body.startTime) req.body.startTime = '18:00:00'
+// Create
+router.post('/', (req, res, next) => {
+  if (!req.body.startTime) req.body.startTime = '18:00:00';
+
   knex('events')
     .insert(req.body)
     .returning(['id', 'date', 'startTime', 'venue', 'headliner', 'support1', 'support2', 'support3', 'headlinerImgLink', 'headlinerBio', 'meetsCriteria', 'isDenied', 'external'])
-  .then((data) => {
-    res.status(200).json(data[0])
-  })
-})
+    .then(data => res.status(200).json(data[0]));
+});
 
-router.patch('/:id', function(req, res, next){
-  console.log('is events.patch getting hit? ', req.params)
+// Update
+router.patch('/:id', (req, res, next) => {
   knex('events')
     .where('id', req.params.id)
     .update(req.body)
     .returning(['id', 'date', 'startTime', 'venue', 'headliner', 'support1', 'support2', 'support3', 'headlinerImgLink', 'headlinerBio', 'meetsCriteria', 'isDenied', 'external'])
-  .then((data) => {
-    res.status(200).json(data[0])
-  })
-})
+    .then(data => res.status(200).json(data[0]));
+});
 
-//Delete (delete one of the resource)
-// router.delete('/:id', function(req, res, next){
+// Delete
+// router.delete('/:id', (req, res, next) => {
 //   knex('events')
 //     .where('id', req.params.id)
 //     .del('*')
 //     .returning(['id', 'date', 'startTime', 'venue', 'headliner', 'support1', 'support2', 'support3', 'headlinerImgLink', 'headlinerBio', 'meetsCriteria', 'isDenied', 'external'])
-//   .then((data) => {
-//     res.status(200).json(data[0])
-//   })
-// })
+//     .then(data => res.status(200).json(data[0]));
+// });
+
 module.exports = router;
